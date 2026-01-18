4dcbc2f883b136c49dc5642293ddb9fa69af9831
mohdafnaan <mohdafnaan833@gmail.com>
2026-01-18 17:22:50 +0530

    todo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,11 +27,14 @@ function App () {
       onClick={addTodo} className="bg-blue-500 rounded-xl m-2 p-2 text-white cursor-pointer">Add</button>
       <hr className="border-b-4 border-indigo-500 mx-13"/>
      </div>
-     <ol className="flex flex-col list-decimal  p-2 m-2 ">
+     <ol className="flex flex-col p-2 m-2 ">
       {
         todos.map((item,index)=>(
           
-          <li key={index} className="font-semibold">{item}</li>
+          // <div className="border-1 p-2 m-2">
+          <li key={index} className="font-semibold border-1 rounded p-2 m-2">{item}</li>
+
+          // </div>
         ))
       }
      </ol>
